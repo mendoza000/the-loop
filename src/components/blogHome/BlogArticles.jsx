db2eec7656fd4c3157a708e120ec01f1b4474896
Omar Mendoza <59121837+mendoza000@users.noreply.github.com>
2022-02-25 19:34:47 -0400

    Añadidos articulos de ejemplo en el home

---
+++ src/components/blogHome/BlogArticles.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+export const BlogArticles = () => {
+	return(
+		<div className="home__articles">
+			<h1 className="home__articles-title">
+				<box-icon name="time" type="solid"></box-icon>
+				Articulos nuevos
+			</h1>
+
+			<div className="home__articles-article">
+				<div className="home__articles-article-decoration"></div>
+
+				<h3>
+					¿Es importante saber usar la terminal?
+				</h3>
+
+			</div>
+
+			<div className="home__articles-article">
+				<div className="home__articles-article-decoration"></div>
+				
+				<h3>
+					¿Es importante saber usar la terminal?
+				</h3>
+
+			</div>
+
+			<div className="home__articles-article">
+				<div className="home__articles-article-decoration"></div>
+				
+				<h3>
+					¿Es importante saber usar la terminal?
+				</h3>
+
+			</div>
+
+			<div className="home__articles-article">
+				<div className="home__articles-article-decoration"></div>
+				
+				<h3>
+					¿Es importante saber usar la terminal?
+				</h3>
+
+			</div>
+
+
+		</div>
+	)
+}
--- src/components/blogHome/BlogHome.jsx
+++ src/components/blogHome/BlogHome.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BlogHeader } from './BlogHeader';
+import { BlogArticles } from './BlogArticles'
 
 export const BlogHome = () => {
 	return(
 		<div className="animate__animated animate__fadeIn animate__faster">
 			<BlogHeader />
+			<BlogArticles />
 		</div>
 	)
 }
